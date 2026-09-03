f39c8fa84e6e582e32e069a7fcc967a951b2fcec
asmitak1109 <korbuasmita@gmail.com>
2026-09-03 23:23:26 +0530

    First commit

---
+++ EmailDemo.java
@@ -0,0 +1,6 @@
+public class EmailDemo {
+    public static void main(String[] args) {
+        System.out.println(" I am full stack java developer. I am happy. I am healthy. I am glowing.");
+    }
+}
+
