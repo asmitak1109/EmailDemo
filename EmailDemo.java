a8402aa2ebe8f1aaefa77c7a96c318dc806fa7ea
asmitak1109 <korbuasmita@gmail.com>
2026-09-04 12:31:09 +0530

    Fix formatting in EmailDemo.java

--- EmailDemo.java
+++ EmailDemo.java
@@ -1,5 +1,5 @@
 public class EmailDemo {
     public static void main(String[] args) {
         System.out.println(" I am full stack java developer. I am happy. I am healthy. I am glowing.");
-    }
+    
 }
